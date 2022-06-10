1586c17e56a5d744e370ce25b2486755ca7999d1
Andrew Gomez <drewgomez209@gmail.com>
2022-06-10 16:24:31 -0700

    getting actions

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,15 +1,5 @@
 const { get } = require('./projects-model')
 
-function count(req, res, next) {
-    if (req.count == null) {
-        req.count = 0;
-    } else {
-        req.count++;
-    }
-    console.log(req.count);
-    next();
-}
-
 function validateId(req, res, next) {
     const { id } = req.params;
     get(id)
@@ -22,31 +12,6 @@ function validateId(req, res, next) {
         })
 }
 
-function postProject(req, res, next) {
-    const { name, description } = req.body;
-    if (req.body) {
-        next()
-    } else {
-        res.status(400).json({ message: "Missing required fields" })
-    }
-}
-
-function updateProjects(req, res, next) {
-    const { completed } = req.body;
-    if (completed === undefined) {
-        res.status(400).json({ message: "Completed status is required!" });
-    } else {
-        next();
-    }
-}
-
-function deleteProject(req, res, next) {
-
-}
-
 module.exports = {
-    validateId,
-    count,
-    postProject,
-    updateProjects
+    validateId
 }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,11 +1,11 @@
 // Write your "projects" router here!
 const express = require('express')
 const { get, insert, update, remove, getProjectActions } = require('./projects-model')
-const { validateId, count, postProject, updateProjects } = require('./projects-middleware')
+const { validateId } = require('./projects-middleware')
 
 const projectsRouter = express.Router()
 
-projectsRouter.get('/', count, (req, res) => {
+projectsRouter.get('/', (req, res) => {
     get()
         .then(project => {
             res.json(project)
@@ -23,10 +23,13 @@ projectsRouter.get('/:id', validateId, (req, res) => {
             .then(project => {
                 res.json(project)
             })
+            .catch(() => {
+                res.status(500).json({ message: "Getting id not working" })
+            })
     }
 });
 
-projectsRouter.post('/', postProject, (req, res) => {
+projectsRouter.post('/', (req, res) => {
     const newProject = req.body
     insert(newProject)
         .then(project => {
@@ -37,7 +40,7 @@ projectsRouter.post('/', postProject, (req, res) => {
         })
 })
 
-projectsRouter.put("/:id", validateId, postProject, updateProjects, (req, res) => {
+projectsRouter.put("/:id", validateId, (req, res) => {
     const { name, description, completed } = req.body
     if (!name || !description || typeof completed !== 'boolean') {
         res.status(400).json({ message: 'Project id doesnt exist' })
@@ -59,6 +62,16 @@ projectsRouter.delete('/:id', validateId, (req, res) => {
         })
         .catch(() => {
             res.status(500).json({ message: 'Failed to delete project' })
+        })
+})
+
+projectsRouter.get('/:id/actions', validateId, (req, res) => {
+    getProjectActions(req.params.id)
+        .then(action => {
+            res.json(action)
+        })
+        .catch(() => {
+            res.status(500).json({ message: "No action for you" })
         })
 })
 
