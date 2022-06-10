dc24fab0d908037eb4bbafa28b06031b4870462f
Andrew Gomez <drewgomez209@gmail.com>
2022-06-10 09:48:02 -0700

    h1 showing

--- api/server.js
+++ api/server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const server = express();
+const cors = require('cors');
+
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send(`<h1>${process.env.MESSAGE}</h1>`)
+})
+
+server.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message, stack: err.stack })
+})
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
